79b5fccafec6856d04868b1455c91a168330904d
Carol Duarte <carolduarte2000@gmail.com>
2025-02-12 10:23:56 -0300

    [feat] Adiciona validações de entrada

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -1,32 +1,48 @@
 ﻿using Layout;
 using GerenciarTarefa;
 
+bool entradaValida;
+
 Console.Clear();
-menu:
-Formatacao.ImprimirCabecalho();
-Console.WriteLine("1 – Adicionar Tarefa");
-Console.WriteLine("2 – Listar Tarefas");
-Console.WriteLine("3 – Concluir Tarefa");
-Console.WriteLine("4 – Remover Tarefa");
-Console.WriteLine("0 – Sair");
-Console.Write("Escolha uma opção: ");
-string opcao = Console.ReadLine();
-switch (opcao)
+
+while (true)
 {
-    case "1":
-        GerenciadorTarefas.AdicionarTarefa();
-        break;
-    case "2":
-        GerenciadorTarefas.ListarTarefas();
-        break;
-    case "3":
-        GerenciadorTarefas.ConcluirTarefa();
-        break;
-    case "4":
-        GerenciadorTarefas.RemoverTarefa();
-        break;
-    case "0":
-        Formatacao.Cor("Saindo...", ConsoleColor.Red);
-        return;
+    Formatacao.ImprimirCabecalho();
+    Console.WriteLine("1 – Adicionar Tarefa");
+    Console.WriteLine("2 – Listar Tarefas");
+    Console.WriteLine("3 – Concluir Tarefa");
+    Console.WriteLine("4 – Remover Tarefa");
+    Console.WriteLine("0 – Sair");
+    do
+    {
+        Console.Write("Escolha uma opção: ");
+        string opcao = Console.ReadLine();
+        switch (opcao)
+        {
+            case "1":
+                entradaValida = true;
+                GerenciadorTarefas.AdicionarTarefa();
+                break;
+            case "2":
+                entradaValida = true;
+                GerenciadorTarefas.ListarTarefas();
+                break;
+            case "3":
+                entradaValida = true;
+                GerenciadorTarefas.ConcluirTarefa();
+                break;
+            case "4":
+                entradaValida = true;
+                GerenciadorTarefas.RemoverTarefa();
+                break;
+            case "0":
+                Console.WriteLine();
+                Formatacao.Cor("Saindo...", ConsoleColor.Red);
+                return;
+            default:
+                entradaValida = false;
+                Formatacao.Cor("Opção inválida! Tente novamente.", ConsoleColor.Magenta);
+                break;
+        }
+    } while (!entradaValida);
 }
-goto menu;
